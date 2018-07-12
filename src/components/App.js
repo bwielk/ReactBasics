0dfe5870a65b5b2448e7cc9bc907d509fb01eb1e
bwielk <bwielk@gmail.com>
2018-07-12 21:42:58 +0100

    Creating components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import Person from './Person.js';
 
-class App extends Component {
-  render() {
-    return (
-        <header>
-          <p>Hello</p>
-        </header>
-    );
-  }
+function Person(){
+	return(
+		<div>
+			<h1>Name</h1>
+			<p>Age</p>
+		</div>
+		);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+function Person(props){
+	return(
+		<div>
+			<h1>Name: {props.name} {props.surname}</h1>
+			<p>Age: {props.age}</p>
+			<p>Occupation: {props.occupation}</p>
+		</div>
+		);
+}
+
+let people = (
+	<div>
+		<Person name="Blazej" 
+						surname="Wielk" 
+						age="26" 
+						occupation="UX designer"/>
+		<Person name="Joni" 
+						surname="Mitchell" 
+						age="76"
+						occupation="Composer"/>
+	</div>
+)
+
+ReactDOM.render(people, document.getElementById('app'));
